5fd9f1bca03b598ca6c04054baabcbc8a9051c35
Vitaly Park <vitalypark@Vitalys-MacBook-Pro.local>
2023-05-22 15:55:05 -0700

    sidebar sales

--- cypress/support/nopCommerce/sidebar_page.js
+++ cypress/support/nopCommerce/sidebar_page.js
@@ -66,5 +66,58 @@ export class SideBarPage {
         cy.contains("Edit").should("be.visible");
         cy.contains("Show").should("be.visible");
     }
+    assertOrdersPage() {
+        cy.contains("Orders").should("be.visible");
+        cy.contains("Start date").should("be.visible");
+        cy.contains("End date").should("be.visible");
+        cy.contains("Order status").should("be.visible");
+        cy.contains("Payment status").should("be.visible");
+        cy.get("label").should("contain", "Payment statuses");
+    }
+
+    assertShipmentsPage() {
+        cy.contains("Shipments").should("be.visible");
+        cy.contains("Start date").should("be.visible");
+        cy.contains("End date").should("be.visible");
+        cy.contains("Shipment #").should("be.visible");
+        cy.contains("Order #").should("be.visible");
+        cy.contains("Pickup from store").should("be.visible");
+    }
+
+    assertReturnRequestsPage() {
+        cy.contains("Return requests").should("be.visible");
+        cy.contains("Start date").should("be.visible");
+        cy.contains("End date").should("be.visible");
+        cy.contains("Customer").should("be.visible");
+        cy.contains("Quantity").should("be.visible");
+        cy.contains("ID").should("be.visible");
+    }
+
+    assertRecurringPaymentsPage() {
+        cy.contains("Recurring payments").should("be.visible");
+        cy.contains("Customer").should("be.visible");
+        cy.contains("ID").should("be.visible");
+        cy.contains("Cycle length").should("be.visible");
+        cy.contains("Cycle period").should("be.visible");
+        cy.get("th").should("contain", "Is active");
+    }
+
+    assertGiftCardsPage() {
+        cy.contains("Gift cards").should("be.visible");
+        cy.contains("Recipient name").should("be.visible");
+        cy.contains("Activated").should("be.visible");
+        cy.contains("Gift card coupon code").should("be.visible");
+        cy.contains("Initial value").should("be.visible");
+        cy.get("th").should("contain", "Remaining amount");
+    }
+
+    assertShoppingCartsAndWishlistsPage() {
+        cy.contains("Shopping carts and wishlists").should("be.visible");
+        cy.contains("Start date").should("be.visible");
+        cy.contains("End date").should("be.visible");
+        cy.contains("Shopping cart type").should("be.visible");
+        cy.contains("Billing country").should("be.visible");
+        cy.get("th").should("contain", "Total items");
+    }
 }
 export const onSideBarPage = new SideBarPage();
